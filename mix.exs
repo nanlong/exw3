c874c2637b99cb1bc39b235e000853574428b820
nanlong <200006506@qq.com>
2020-04-01 17:54:04 +0800

    update deps version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExW3.MixProject do
     [
      app: :exw3,
      version: "0.4.4",
-     elixir: "~> 1.7.2",
+     elixir: "~> 1.10",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -38,7 +38,7 @@ defmodule ExW3.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:ethereumex, "~> 0.4.0"},
+      {:ethereumex, "~> 0.6"},
       {:exth_crypto, "~> 0.1.6"},
       {:abi, "~> 0.1.8"},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
